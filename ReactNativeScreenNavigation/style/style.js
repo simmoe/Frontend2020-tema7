a0698863897488a7ac12784a214e701108f215f0
Simon Moe <simmoe@gmail.com>
2020-03-12 15:38:49 +0100

    1st commit again

---
+++ ReactNativeScreenNavigation/style/style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+export const globalStyles = StyleSheet.create({
+    centered:{
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    primaryBackground:{
+        backgroundColor: '#4285F4',
+    },
+    primaryText:{
+        color:'white',
+        fontSize:14,
+    }
+})
