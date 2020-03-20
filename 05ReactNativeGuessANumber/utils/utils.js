9267f24b7a05271e699a962f15d0776e0afc7760
Simon Moe <simmoe@gmail.com>
2020-03-20 15:10:37 +0100

    deleted every little thing

--- 05ReactNativeGuessANumber/utils/utils.js
+++
@@ -1,8 +0,0 @@
-export const generateRandomNumber = (min, max) => {
-    min = Math.ceil(min) + 1 
-    max = Math.floor(max) - 1
-
-    const random = Math.floor( Math.random() * (max - min) ) + min
-
-    return random
-}
--- svelte-native-api-template/Readme.md
+++
@@ -1,3 +0,0 @@
-# API template
-
-This project can get you started building your own data driven native app with svelte-native. Download zip, run npm install and don't forget to obtain your own api key.    
