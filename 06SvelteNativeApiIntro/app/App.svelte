2e9291b05680f5c6fe941a30302f246c1bec88d0
Simon Moe <simmoe@gmail.com>
2020-03-20 10:15:02 +0100

    localstorage

--- 06SvelteNativeApiIntro/app/App.svelte
+++ 06SvelteNativeApiIntro/app/App.svelte
@@ -1,20 +1,37 @@
 <script>
     import { onMount } from 'svelte'
+    const utilsModule = require('tns-core-modules/utils/utils')
+    const appSettings = require('tns-core-modules/application-settings')
     import { showModal } from 'svelte-native'
     import Article from './modals/Article.svelte'
 
+    let countryNumber = 0
+    let country = 'us'
+    $: {
+        country = countryNumber == 0 ? 'us' : 'no'
+        getData()
+    }
+
     const api_key = 'ed87ebff01bb4f5998eed5fb8a0aba89'
-    const url = `https://newsapi.org/v2/top-headlines?country=no&apiKey=${api_key}`
 
-    let articles = []
-
-    onMount(() => {
+    const getData = () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${api_key}`
         fetch(url)
             .then( response => response.json() )
             .then( json => {
                 articles = json.articles
             })
             .catch( error => console.log(error) )
+    } 
+    const test = 0
+
+    let articles = []
+
+    onMount(() => {
+        if(appSettings.getNumber('default-country')){
+            countryNumber = appSettings.getNumber('default-country')
+        }
+        getData()
     })
 
     const showArticle = async (article) => {
@@ -27,22 +44,35 @@
             }
         )
     }
-
+    const setDefaultCountry = () => {
+        appSettings.setNumber('default-country', countryNumber)
+    }
 </script>
 
 <page>
     <actionBar title="World News" />
     <stackLayout>
+    <segmentedBar 
+        selectedBackgroundColor='black' 
+        style='margin: 12;' 
+        bind:selectedIndex={countryNumber}>
+
+        <segmentedBarItem title="USA" />
+        <segmentedBarItem title="Norway" />
+    </segmentedBar>
+    <button on:tap={setDefaultCountry} text='set default'/>
         <scrollView>
-            <stackLayout class='articles'>            
+            <stackLayout class='articles'>    
                 {#each articles as article}
-                    <stackLayout
+                    <flexBoxLayout
                         on:tap={ () => showArticle(article)} 
-                        class='article'>
-<image class='img-rounded' src='{article.urlToImage}' alt='cover' stretch='aspectFill' />
-<label textWrap="{false}" class='h2 text-center white' text='{article.title}' />
-<label class='body' text='{article.author}'/>
-                    </stackLayout>
+                        class='article'
+                        style="background-image:url('{article.urlToImage}')"
+                        >
+                        <!-- <image class='img-rounded image' height='300' src='{}' alt='cover' stretch='aspectFill' /> -->
+                        
+                        <label textWrap="{true}" class='h2 white article-heading' text='{article.title}' />
+                    </flexBoxLayout>
                 {:else}
                     <activityIndicator busy={true} />
                 {/each}
@@ -58,20 +88,21 @@
     }
     .article{
         padding-bottom:24;
-        animation:leftIn .4s ease;
+        margin:12;
+        height:300;
+        background-size:cover;
+        flex-direction:column;
+        justify-content:flex-end;
+        align-items:center;
+        animation: leftIn .4s ease;
     }
     @keyframes leftIn{
         from{
-            transform:translate(-10,0)
+            transform:translate(-10, -10)
         }
         to{
-            transform:translate(0,0)
+            transform:translate(0, 0)
         }
-    }
-    .h2{
-        margin-left:0;
-        padding-left:0;
-        margin-top:10;
     }
     .white{
         color:white;
